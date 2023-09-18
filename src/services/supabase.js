2054f0b2eccbce8828804ead4d60b8a5ee55426f
Rizky Juni <48887057+rjuniyy@users.noreply.github.com>
2023-09-19 05:30:19 +0700

    Add files via upload

---
+++ src/services/supabase.js
@@ -0,0 +1,28 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_KEY } from '@env';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key, value) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://rtsmdhjrozectbtqlofe.supabase.co';
+const supabaseAnonKey = SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
